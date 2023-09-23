b0353835d64334e749fcd6f3f1d36bd8e99c78ef
Cristian-Tr <106551922+Cristian-Tr@users.noreply.github.com>
2023-09-23 16:35:12 +0300

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  //Menu toggle burger button
+  let menuButton = document.getElementById('burger-menu');
+
+  menuButton.addEventListener('click', function () {
+    menuButton.classList.toggle('fa-bars');
+    menuButton.classList.toggle('fa-times');
+  });
+
+
+ 
+
+
+
+});
